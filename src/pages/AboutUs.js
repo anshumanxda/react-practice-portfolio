8db688aa7f366fbcef510d72df20653a4917661d
anshumanxda <anshumanpathak4@gmail.com>
2021-05-02 23:47:49 +0530

    AboutUs: Faq Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from "react";
 //components
 import AboutSection from "../comonents/AboutSections";
 import ServicesSection from "../comonents/ServicesSection";
+import FaqSection from "../comonents/FaqSection";
 
 const AboutUs = () => {
   return (
     <>
       <AboutSection />;
       <ServicesSection />;
+      <FaqSection />
     </>
   );
 };
