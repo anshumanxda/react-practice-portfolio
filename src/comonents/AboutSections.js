7bd7c640a7c3d11842a3328a5b2cfbcc2dcf6a68
anshumanxda <anshumanpathak4@gmail.com>
2021-05-03 22:45:49 +0530

    Styled Components: AboutSection

--- src/comonents/AboutSections.js
+++ src/comonents/AboutSections.js
@@ -1,33 +1,68 @@
 import React from "react";
 import home1 from "../img/home1.png";
+//import styled component
+import styled from "styled-components";
+
 const AboutUs = () => {
   return (
-    <div>
-      <div className="description">
+    <StyledAbout>
+      <StyledDescription className="description">
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>We work to make</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               your <span>dreams</span>
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>come true.</h2>
-          </div>
+          </Hide>
         </div>
         <p>
-          Contact us for any kind of photography or videography, we have the
-          potential professional to work toward your dream.
+          Contact us for any kind of photography or videography work, we have
+          the potential professionals to work towards your dream.
         </p>
         <button>Contact Us</button>
-      </div>
-      <div className="image">
+      </StyledDescription>
+      <StyledImage className="image">
         <img src={home1} alt="" />
-      </div>
-    </div>
+      </StyledImage>
+    </StyledAbout>
   );
 };
+
+//styled component
+const StyledAbout = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+
+const StyledDescription = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const StyledImage = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutUs;
