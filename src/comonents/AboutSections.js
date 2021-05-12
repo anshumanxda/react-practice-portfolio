4933819bf566f3363fe1cb2abe7d930b8ed1b91d
anshumanxda <anshumanpathak4@gmail.com>
2021-05-12 23:46:51 +0530

    AboutSection: Added animation to hero section

--- src/comonents/AboutSections.js
+++ src/comonents/AboutSections.js
@@ -5,6 +5,7 @@ import home1 from "../img/home1.png";
 import { StyledAbout, StyledDescription, StyledImage, Hide } from "../styles";
 //framer motion
 import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation";
 
 const AboutSection = () => {
   return (
@@ -12,25 +13,25 @@ const AboutSection = () => {
       <StyledDescription className="description">
         <motion.div>
           <Hide>
-            <motion.h2>We work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span>
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>come true.</h2>
+            <motion.h2 variants={titleAnim}> come true.</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contact us for any kind of photography or videography work, we have
           the potential professionals to work towards your dream.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </StyledDescription>
       <StyledImage className="image">
-        <img src={home1} alt="" />
+        <motion.img variants={photoAnim} src={home1} alt="" />
       </StyledImage>
     </StyledAbout>
   );
