a99bb3a4d06a32698b9a9f26b0431b60e95f0b4f
anshumanxda <anshumanpathak4@gmail.com>
2021-05-15 01:56:03 +0530

    OurWork: animation

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -5,11 +5,21 @@ import { Link } from "react-router-dom";
 import athlete from "../img/athlete-small.png";
 import theracer from "../img/theracer-small.png";
 import goodtimes from "../img/goodtimes-small.png";
-//aniamtion
+//animation
 import { motion } from "framer-motion";
-import { pageAnimation, fade, photoAnim, lineAnim, slider } from "../animation";
+import {
+  pageAnimation,
+  fade,
+  photoAnim,
+  lineAnim,
+  slider,
+  scrollReveal,
+} from "../animation";
+import { useScroll } from "../comonents/useScroll";
 
 const OurWork = () => {
+  const [element, controls] = useScroll();
+  const [element2, controls2] = useScroll();
   return (
     <StyledWork
       exit="exit"
@@ -31,16 +41,26 @@ const OurWork = () => {
           </Hide>
         </Link>
       </StyledMovie>
-      <StyledMovie>
+      <StyledMovie
+        ref={element2}
+        animate={controls2}
+        variants={scrollReveal}
+        initial="hidden"
+      >
         <h2>The Racer</h2>
-        <div className="line"></div>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/work/the-racer">
           <img src={theracer} alt="" />
         </Link>
       </StyledMovie>
-      <StyledMovie>
+      <StyledMovie
+        ref={element}
+        animate={controls}
+        variants={scrollReveal}
+        initial="hidden"
+      >
         <h2>Good Times</h2>
-        <div className="line"></div>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/work/good-times">
           <img src={goodtimes} alt="" />
         </Link>
@@ -57,7 +77,7 @@ const StyledWork = styled(motion.div)`
     padding: 1rem 0rem;
   }
 `;
-const StyledMovie = styled.div`
+const StyledMovie = styled(motion.div)`
   padding-bottom: 10rem;
   .line {
     height: 0.5rem;
